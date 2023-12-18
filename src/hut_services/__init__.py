5c6c5aceb238eacf6470e0e4fbf442852b407d01
Tobias Burgherr <tb@wodore.com>
2023-12-18 19:51:25 +0100

    Added __all__

--- src/hut_services/__init__.py
+++ src/hut_services/__init__.py
@@ -12,4 +12,22 @@ from .core.schema.geo import LocationSchema
 from .core.schema.locale import TranslationSchema
 from .core.service import BaseService
 from .osm import OsmService
+from .osm.utils import guess_hut_type
 from .refuges_info import RefugesInfoService
+
+__all__ = [
+    "BaseHutConverterSchema",
+    "BaseHutSourceSchema",
+    "CapacitySchema",
+    "ContactSchema",
+    "HutSchema",
+    "HutSourceSchema",
+    "HutTypeEnum",
+    "SourcePropertiesSchema",
+    "LocationSchema",
+    "TranslationSchema",
+    "BaseService",
+    "OsmService",
+    "RefugesInfoService",
+    "guess_hut_type",
+]
