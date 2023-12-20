62517788b0d463a5565fb5725be4cbb7b19c12c1
Tobias Burgherr <tb@wodore.com>
2023-12-20 12:47:30 +0100

    Moved __all__ to top

--- src/hut_services/__init__.py
+++ src/hut_services/__init__.py
@@ -1,22 +1,3 @@
-from .core.schema import (
-    BaseHutConverterSchema,
-    BaseHutSourceSchema,
-    CapacitySchema,
-    ContactSchema,
-    HutSchema,
-    HutSourceSchema,
-    HutTypeEnum,
-    OwnerSchema,
-    SourcePropertiesSchema,
-)
-from .core.schema.geo import LocationSchema
-from .core.schema.locale import TranslationSchema
-from .core.service import BaseService
-from .osm import OsmService
-from .osm.utils import guess_hut_type
-from .refuges_info import RefugesInfoService
-from .services import SERVICES
-
 __all__ = [
     "BaseHutConverterSchema",
     "BaseHutSourceSchema",
@@ -35,3 +16,22 @@ __all__ = [
     "guess_hut_type",
     "SERVICES",
 ]
+
+from .core.schema import (
+    BaseHutConverterSchema,
+    BaseHutSourceSchema,
+    CapacitySchema,
+    ContactSchema,
+    HutSchema,
+    HutSourceSchema,
+    HutTypeEnum,
+    OwnerSchema,
+    SourcePropertiesSchema,
+)
+from .core.schema.geo import LocationSchema
+from .core.schema.locale import TranslationSchema
+from .core.service import BaseService
+from .osm import OsmService
+from .osm.utils import guess_hut_type
+from .refuges_info import RefugesInfoService
+from .services import SERVICES
