502168e84fd7a29316747148aac512b4ede0c03d
Tobias Burgherr <tb@wodore.com>
2024-01-22 22:46:48 +0100

    Added extra allow

--- src/hut_services/core/schema/_base.py
+++ src/hut_services/core/schema/_base.py
@@ -4,4 +4,4 @@ from pydantic import BaseModel, ConfigDict
 class BaseSchema(BaseModel):
     """Use this as a base for all schemas, sets `from_attributes` to `True`."""
 
-    model_config = ConfigDict(from_attributes=True)
+    model_config = ConfigDict(from_attributes=True, extra="allow")
