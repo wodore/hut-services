9ab1d30d0d7b0ebc57c47d640b70b5d7f5538c4d
Tobias Burgherr <tburgherr@gmail.com>
2024-10-07 23:53:12 +0200

    Corrected comment

--- src/hut_services/wikicommons/service.py
+++ src/hut_services/wikicommons/service.py
@@ -183,7 +183,7 @@ def get_wikicommon_photo_info(
     # if "int-own-work" in source_name:
     #    source_url = cast(HttpUrl, wikicommons_url)
     #    source_name = "wikicommons"
-    comment = "* Wikicommons source: {wikicommons_url}\n"
+    comment = f"* Wikicommons source: {wikicommons_url}\n"
     if "camptocamp" in source_name.lower():
         source_name = "camptocamp"
         match = re.search(r"\d{3,}", cast(str, source_url))
@@ -200,9 +200,9 @@ def get_wikicommon_photo_info(
             source_ident = match.groups()[0] if match else source_ident
     else:
         if source_name:
-            comment = "* Original source name: {source_name}\n"
+            comment = f"* Original source name: {source_name}\n"
         if source_url:
-            comment = "* Original source url: {source_}\n"
+            comment = f"* Original source url: {source_url}\n"
         source_url = cast(HttpUrl, wikicommons_url)
         source_name = "wikicommons"
     # if not source_url:
